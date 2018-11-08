9bad1de253019ec120854db914d6fb67cb9adf35
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-11-08 15:24:15 +0300

    forgot password 9;

--- SocialNetwork.sql
+++ SocialNetwork.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 07 2018 г., 16:49
+-- Время создания: Ноя 08 2018 г., 15:23
 -- Версия сервера: 5.7.20
 -- Версия PHP: 7.0.26
 
@@ -39,7 +39,19 @@ CREATE TABLE `login_tokens` (
 --
 
 INSERT INTO `login_tokens` (`id`, `token`, `user_id`) VALUES
-(2, 'aef8e501b6da2bfcb7df4c90edf69c8408ab7a76', 8);
+(10, '11d3a9e9fe01b97e2c78ec1ad63075848e2c2cc9', 8);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `password_tokens`
+--
+
+CREATE TABLE `password_tokens` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `token` char(64) NOT NULL,
+  `user_id` int(11) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -59,15 +71,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'advocat', '1234', 'sergey_bobkov@inbox.ru'),
-(2, 'Sergeykira', '123456', 'pochta@pktitan.ru'),
-(3, 'admin_advokat', '123456', 'taran.kira@rambler.ru'),
-(4, 'advocat', '1234', 'pochta@pktitan.ru'),
-(5, 'admin_advokat', '123', 'taran.kira@rambler.ru'),
-(6, 'name', '1234', 'name@name.ru'),
-(7, 'username1', '$2y$10$qaK7aP.tp66ASemSfDp0HeQkohA4j2F/0QxXZYWBYzA76UvOHmdZ6', 'sergey_bobkov@inbox.ru'),
-(8, 'Sergey', '$2y$10$t1dzJH6.cdeeQa4mNIBafuXTexnNus1gvGNyokV/NYx1yTxnrTUBS', 'sergey_bobkov@inbox.ru'),
-(9, 'Sergey1', '$2y$10$CCi4Md.RCxVqxtMK.41Jmeb8dX4daYDvkl3feDdTdk/5ugmHXattO', 'sergey_bobkov1@inbox.ru');
+(8, 'Sergey', '$2y$10$Sb7DtRyvi5YDTlD.zOWJUOPsP81Oa.4xzcT31mjNB0odisPeteROK', 'sergey_bobkov@inbox.ru'),
+(10, 'Kira', '$2y$10$bgA2eRCAWdZlYAoSmEuYPOiOEOTclzeMXgzv7DwYoMg3reVZT0tO2', 'taran.kira@rambler.ru');
 
 --
 -- Индексы сохранённых таблиц
@@ -77,6 +82,14 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
 -- Индексы таблицы `login_tokens`
 --
 ALTER TABLE `login_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `password_tokens`
+--
+ALTER TABLE `password_tokens`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `token` (`token`),
   ADD KEY `user_id` (`user_id`);
@@ -95,13 +108,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `login_tokens`
 --
 ALTER TABLE `login_tokens`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT для таблицы `password_tokens`
+--
+ALTER TABLE `password_tokens`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
@@ -112,6 +131,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `login_tokens`
   ADD CONSTRAINT `login_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `password_tokens`
+--
+ALTER TABLE `password_tokens`
+  ADD CONSTRAINT `password_tokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
